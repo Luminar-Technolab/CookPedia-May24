1b50da604f256b387be8a17c95922dd2243b592f
soniagkalliath <soniageorge111@gmail.com>
2024-11-14 17:10:48 +0530

    recipe completed

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { ApiService } from '../services/api.service';
+import { SearchPipe } from '../pipes/search.pipe';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [HeaderComponent,HeaderComponent],
+  imports: [HeaderComponent,SearchPipe,FormsModule,NgxPaginationModule],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
 export class RecipesComponent {
+  p: number = 1;
   allRecipes:any = []
+  dummyAllRecipes:any = []
+  searchKey:string = ""
 
   constructor(private api:ApiService){}
   
@@ -21,7 +27,14 @@ export class RecipesComponent {
   getAllRecipes(){
     this.api.getAllRecipeAPI().subscribe((res:any)=>{
       this.allRecipes = res
+      this.dummyAllRecipes = this.allRecipes
       console.log(this.allRecipes);      
     })
   }
+
+
+  filterRecipes(recipeType:string,recipeName:string){
+    this.allRecipes = this.dummyAllRecipes.filter((item:any)=>item[recipeType].includes(recipeName))
+  }
+
 }
